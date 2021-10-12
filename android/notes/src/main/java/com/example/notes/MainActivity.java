116ccfa91ad746da07868274a0a9b5e5b7e8babf
Xia Zhao <zhaoxia9406@gmail.com>
2021-10-12 13:52:57 -0700

    [notes][Logcat] How to use Logcat to debug and how to write log in the app.

--- android/notes/src/main/java/com/example/notes/MainActivity.java
+++ android/notes/src/main/java/com/example/notes/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.notes;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "NotesActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate:this is a test of log output.");
     }
 }
